ad3d10c02005c9d37e416a9e69c24a34f62b20aa
rsbassi9 <rajvir.bassi95@gmail.com>
2023-11-16 11:26:40 -0800

    Begin building blockchain class

--- Module 1 - Create a Blockchain/blockchain.py
+++ Module 1 - Create a Blockchain/blockchain.py
@@ -14,3 +14,12 @@ import json
     # From flask, we will need the Flask class to create an object of the flask class to make the web application 
     # jsonify is a function used to return the messages in postman when we interact with postman
 from flask import Flask, jsonify
+
+# Part 1 - Building a Blockchain
+class Blockchain:
+    
+    def __init__(self):
+        self.chain = []     # The chain containing the blocks initialized as a list
+        self.create_block(proof = 1, previous_hash = '0')    # Genesis block, created using the create_block function. arguments: proof = arbitrary number, previous hash (arbitraty for genesis block)
+
+# Part 2 - Mining our Blockchain
