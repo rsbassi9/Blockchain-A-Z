a22f8858ed4b1fbb03b11f0650fb6577784f53d6
rsbassi9 <rajvir.bassi95@gmail.com>
2023-11-17 13:17:59 -0800

    Added request to check if the blockchain is valid"

--- Module 1 - Create a Blockchain/blockchain.py
+++ Module 1 - Create a Blockchain/blockchain.py
@@ -112,6 +112,16 @@ def get_chain():
                 'length' : len(blockchain.chain)}   # We will also retreive the length of the blockchain so that we can keep track of it as it grows
     return jsonify(response), 200
 
+# Checking if the Blockchain is valid
+@app.route('/is_valid', methods=['GET'])
+def is_valid():
+    is_valid = blockchain.is_chain_valid(blockchain.chain)
+    if is_valid:
+        response = {'message' : 'All good. The block chain is valid.'}
+    else:
+        response = {'message' : 'Houston, we have a problem. The Blockchain is not valid' }
+    return jsonify(response), 200
+    
 # Running the app
 app.run(host = '0.0.0.0', port = 5000)
 
